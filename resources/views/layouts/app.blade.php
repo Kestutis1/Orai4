d4e9d1dc313a9b129af008c1f7f6c28d7ebffbac
Kestutis1 <djkestutis@yahoo.com>
2019-11-11 09:43:30 +0200

    nesvarbu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,11 +24,12 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Šiaip sau svetainė') }}
+                    Šiaip sau svetainė
+                    <!-- {{ config('app.name', 'Šiaip sau svetainė') }} -->
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                </button>
+                </button> -->
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
